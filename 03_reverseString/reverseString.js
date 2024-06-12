0183fcb91dab06b85044dda6bcf40da8c9b00b60
Denselle0311 <144681965+Denselle0311@users.noreply.github.com>
2024-06-13 01:50:44 +0800

    passed

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+    const array = word.split('');
+    const reverse = array.reverse();
+    word = reverse.join('');
+    return word;
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr,...removeArr) {
+  let filter =  arr.filter(item => {
+        if(!removeArr.includes(item)) {
+          return item
+        }
+        
+    });
+    return filter;
+   
 };
 
 // Do not edit below this line
