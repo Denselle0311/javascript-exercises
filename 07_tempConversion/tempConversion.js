fb81df89ac254fc17ade8efbacf589a2a1d48fb7
Denselle0311 <144681965+Denselle0311@users.noreply.github.com>
2024-06-13 17:44:11 +0800

    passed

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  let celsius = temp - 32;
+  let rounded = Math.round(celsius*5)/9;
+  return parseFloat(rounded.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  return Math.round(temp*9)/5+32;
 };
 
 // Do not edit below this line
