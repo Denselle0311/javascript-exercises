2f8f645efdac1223562d85365d35ef253de00e28
Denselle0311 <144681965+Denselle0311@users.noreply.github.com>
2024-06-13 13:56:48 +0800

    passed

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,6 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    let centuryYear =  year % 400 === 0 || year % 100 !== 0;
+    return year % 4 === 0 && centuryYear;
 };
 
 // Do not edit below this line
