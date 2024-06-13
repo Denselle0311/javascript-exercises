6c5262076508ac946b00081cf231cfce06a06e60
Denselle0311 <144681965+Denselle0311@users.noreply.github.com>
2024-06-13 12:09:52 +0800

    passed

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
+const sumAll = function(num, times) {
+    let sum = 0;
 
+    if(num < 0 || typeof times !== 'number') 
+    {
+       return sum = 'ERROR';
+    } else if (num > times) 
+    {
+        [num,times] = [times,num]
+    }
+    for (let i = num; i <= times; i++) {
+            sum += i;
+        }
+    return sum;
 };
 
 // Do not edit below this line
