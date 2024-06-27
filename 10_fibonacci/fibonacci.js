92ddd7698ef0f1de10f12813213503c0eb6d1c3f
Denselle0311 <144681965+Denselle0311@users.noreply.github.com>
2024-06-27 21:20:15 +0800

    passed

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,17 @@
-const fibonacci = function() {
-
-};
+const fibonacci = function(num) {
+    Number(num);
+    if(num < 0) return 'OOPS';
+    
+    let acc = 0;
+    let current = 0;
+    let prev = 1;
+    for(let i = 0; i < num; i++) {
+      acc = current + prev;
+      prev = current;
+      current = acc;
+    }
+    return current
+  };
 
 // Do not edit below this line
 module.exports = fibonacci;
