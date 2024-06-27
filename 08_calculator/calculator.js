6e2baa777c4b4f8cfbf9c2f752c00c81dd1a2b0a
Denselle0311 <144681965+Denselle0311@users.noreply.github.com>
2024-06-27 19:30:48 +0800

    passed

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,35 @@
-const add = function() {
-	
+const add = function(...arr) {
+	return +arr.reduce((total,num) => Number(total + num));
 };
 
-const subtract = function() {
-	
+const subtract = function(...arr) {
+	return +arr.reduce((total,num) => Number(total - num));
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  return arr.reduce((total,num) => total + num,0)
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((total,num) => total * num);
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+  let p = a;
+  for(let i = 1; i < b; i++) {
+   p *= a
+  }
+	return p;
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+  if(n == 0 ) return 1;
+  let p = n;
+  for(let i = 1; i < n; i++) {
+    
+    p *= i
+  }
+	return p;
 };
 
 // Do not edit below this line
