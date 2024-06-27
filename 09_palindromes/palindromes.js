d4236cf6c1b035e4075727cc763314824432c9f2
Denselle0311 <144681965+Denselle0311@users.noreply.github.com>
2024-06-27 20:37:55 +0800

    passed

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+//     let temp = str.split(',').join('').split(' ')
+//                   .join('').toLowerCase().split('').filter(e => e != '.' && e != '!').reverse().join('');
+//     let strClone = str.split(',').join('').split(' ')
+//                   .join('').toLowerCase().split('').filter(e => e != '.' && e != '!').join('');
+const cleanStr = str.toLowerCase()
+                      .split('').filter(e => e != ' ' && e != ',' && e != '!' && e != '.').join('');
+  const reverse = Array.from(cleanStr).reverse().join('');
+  return reverse == cleanStr ? true : false;
 };
 
 // Do not edit below this line
