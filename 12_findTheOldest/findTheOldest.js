fd570b58ea733f93bbb92e0bfe8d70af6a10868a
Denselle0311 <144681965+Denselle0311@users.noreply.github.com>
2024-06-28 12:38:16 +0800

    passed

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,14 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) {
+  return arr.reduce((prev,curr) => 
+    getAge(prev.yearOfBirth,prev.yearOfDeath) < getAge(curr.yearOfBirth,curr.yearOfDeath) ? curr : prev
+  )
 };
+
+function getAge(birthDate,deathDate) {
+  let thisYear = new Date().getFullYear();
+  deathDate = deathDate ? deathDate : thisYear;
+  return deathDate - birthDate
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
